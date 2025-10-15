d761b62e4fa30e8337552d5159064dfe8d4a4a04
Redstonedust653 <rattler-surf05@icloud.com>
2025-10-14 21:23:31 -0700

    Update MARKOV_CHAIN.py to include small tutorial

--- MARKOV_CHAIN.py
+++ MARKOV_CHAIN.py
@@ -5,3 +5,10 @@ l = ['String 1', 'String 2']
 chain = ai_tools.markov_chain('word',l,3)
 
 print(chain.generate())
+
+# EXPLANATION TIME:
+# Argument one is mode. it can be 'word' or 'char'. This is for the n-grams.
+# Argument two is a list of str. It does not matter what order the strings are.
+# Argument three is what n for the n-gram.
+
+# and chain.generate() (or whatever you name your markov_chain) will return a str.
